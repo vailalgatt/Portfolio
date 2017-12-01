5209636e56c41e0c9330c5901dde2ad2f2147f5a
vail.algatt <vail.algatt@gmail.com>
2017-12-01 15:48:50 -0700

    fixing up the about component content

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,26 +4,34 @@
     <v-container fluid grid-list-md>
       <v-layout row wrap>
         <v-flex d-flex xs12 sm6 md4>
-          <v-card color="blue lighten-3" dark>
-            <v-card-title primary class="title">About</v-card-title>
-            <v-card-text
-              v-text="about">
-            </v-card-text>
+          <v-card class="border-card">
+            <v-card-title primary class="title">OVERVIEW</v-card-title>
+                <v-card-text
+                  v-text="location">
+                </v-card-text>
+                <v-card-text
+                  v-text="workChippy">
+                </v-card-text>
+               <v-card-text
+                v-text="additionalInformation">
+              </v-card-text>
           </v-card>
         </v-flex>
+
+
 <!-- education section -->
         <v-flex d-flex xs12 sm6 md4>
           <v-layout row wrap>
             <v-flex d-flex>
-              <v-card color="grey darken-2" dark >
-                <v-card-title primary class="title text-xs-center">Education</v-card-title>
+              <v-card color="blue-grey" dark>
+                <v-card-title primary class="title">Education</v-card-title>
                   <v-card-text
                     v-text="education">
                   </v-card-text>
               </v-card>
             </v-flex>
             <v-flex d-flex>
-              <v-card color="grey darken-2" dark >
+              <v-card color="blue-grey lighten-1" dark>
                 <v-card-text
                   v-text="educationPCC">
                 </v-card-text>
@@ -33,10 +41,7 @@
               <v-layout row wrap>
                 <v-flex d-flex
                 >
-                  <v-card
-                    color="grey darken-2"
-                    dark
-                  >
+              <v-card color="blue-grey lighten-1" dark>
                     <v-card-text
                       v-text="educationCW">
                     </v-card-text>
@@ -49,11 +54,16 @@
 
 
         <v-flex d-flex xs12 sm6 md4 child-flex>
-          <v-card color="blue lighten-3" dark>
+          <v-card class="border-card">
             <v-card-title primary class="title">Volunteer</v-card-title>
               <v-card-text
                 v-text="MSC">
               </v-card-text>
+               <v-card-media
+                src="/src/img/msc.png"
+                height="250px"
+                contain>
+              </v-card-media>
           </v-card>
         </v-flex>
 
@@ -62,46 +72,13 @@
        <v-flex d-flex xs12 sm6 md12 child-flex>
           <v-layout row wrap>
             <v-flex d-flex>
-              <v-card color="grey darken-2" dark>
-                <v-card-title primary class="title">Additional Information</v-card-title>
-                <v-card-text class="text-xs-center"
-                  v-text="work">
-                </v-card-text>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-flex>
-       <v-flex d-flex xs12 sm6 md4 child-flex>
-          <v-layout row wrap>
-            <v-flex d-flex>
-          <v-card color="blue lighten-3" dark>
-                <v-card-text
-                  v-text="workChippy">
-                </v-card-text>
+              <v-card color="blue-grey" dark>
+                <v-card-title primary class="title" v-text="available"></v-card-title>
               </v-card>
             </v-flex>
           </v-layout>
         </v-flex>
              
-        <v-flex d-flex xs12 sm6 md4 child-flex>
-          <v-layout row wrap>
-            <v-flex d-flex>
-          <v-card color="blue lighten-3" dark>
-                <v-card-text
-                  v-text="available">
-                </v-card-text>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-flex>
-
-        <v-flex d-flex xs12 sm6 md4 child-flex>
-          <v-card color="blue lighten-3" dark>
-            <v-card-text
-              v-text="additionalInformation">
-            </v-card-text>
-          </v-card>
-        </v-flex>
       </v-layout>
     </v-container>
   </v-app>
@@ -112,17 +89,16 @@
 export default {
   name: 'About',
     data: () => ({
-      about: `Based in Mountain Home AFB, ID.`, 
-      MSC: `I am a chapter leader with MilSpouseCoders 
-              `,
+      location: `Based in Moutain Home AFB, ID`,
+      MSC: `Chapter leader with MilSpouseCoders`,
       education: ``,
       work: ``,        
       educationPCC: `I hold a Computer Information Systems degree from Portland Community College.`,
       educationCW: `I'm a graduate of Boise CodeWorks Immersive Full-Stack. `,
-      workChippy: `Owner and founder of the consulting and customer software development company Chippy Solutions. Call or Email to make an appointment for your first consultation`,
+      workChippy: `Owner and founder of the consulting and custom software development company Chippy Solutions. Call or Email to make an appointment for your first consultation`,
       additionalInformation: `I am a military spouse. Coffee fiend. Dog mom. STOTT certified Pilates instructor. And long time violinist.`,
       available: `I'm currently available to take new clients`
-      })
+      }),
     }
     
 </script>
@@ -133,6 +109,19 @@ export default {
   padding-top: 25px;
   padding-bottom: 0px;
   margin-bottom: 0px;
+}
+
+.bg-img {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background: url('/src/img/location.jpg') center center no-repeat;
+  background-size: cover;
+}
+
+.border-card{
+  border: solid black thin;
+  padding: 15px;
 }
 
 </style>
