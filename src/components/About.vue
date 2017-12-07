17c775414082e814224c2f2f24f74c4591d43664
vail.algatt <vail.algatt@gmail.com>
2017-12-06 19:50:48 -0700

    particles acting a little funky. about section refactored!

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,142 +1,96 @@
 <template>
   <div class="about">
-  <v-app id="inspire">
-    <v-container fluid grid-list-md>
-      <v-layout row wrap>
-        <v-flex d-flex xs12 sm6 md4>
-          <v-card class="border-card">
-            <v-card-title primary class="title">OVERVIEW</v-card-title>
-                <v-card-text
-                  v-text="location">
-                </v-card-text>
-                <v-card-text
-                  v-text="workChippy">
-                </v-card-text>
-               <v-card-text
-                v-text="additionalInformation">
-              </v-card-text>
-          </v-card>
-        </v-flex>
-
-
-<!-- education section -->
-        <v-flex d-flex xs12 sm6 md4>
-          <v-layout row wrap>
-            <v-flex d-flex>
-              <v-card color="blue-grey" dark>
-                <v-card-title primary class="title">Education</v-card-title>
-                  <v-card-text
-                    v-text="education">
-                  </v-card-text>
-              </v-card>
-            </v-flex>
-            <v-flex d-flex>
-              <v-card color="blue-grey lighten-1" dark>
-                <v-card-text
-                  v-text="educationPCC">
-                </v-card-text>
-              </v-card>
-            </v-flex>
-            <v-flex d-flex>
-              <v-layout row wrap>
-                <v-flex d-flex
-                >
-              <v-card color="blue-grey lighten-1" dark>
-                    <v-card-text
-                      v-text="educationCW">
-                    </v-card-text>
-                  </v-card>
-                </v-flex>
-              </v-layout>
-            </v-flex>
-          </v-layout>
-        </v-flex>
-
-
-        <v-flex d-flex xs12 sm6 md4 child-flex>
-          <v-card class="border-card">
-            <v-card-title primary class="title">Volunteer</v-card-title>
-              <v-card-text
-                v-text="MSC">
-              </v-card-text>
-               <v-card-media
-                src="/src/img/msc.png"
-                height="250px"
-                contain>
-              </v-card-media>
-          </v-card>
-        </v-flex>
-
-
-<!-- work section -->
-       <v-flex d-flex xs12 sm6 md12 child-flex>
-          <v-layout row wrap>
-            <v-flex d-flex>
-               <v-card class="change">
-                <v-card-title primary class="title" v-text="available"></v-card-title>
-              </v-card>
-            </v-flex>
-          </v-layout>
-        </v-flex>
-             
-      </v-layout>
-    </v-container>
-  </v-app>
+        <div class="wrap-banner">
+          <div class="main-title">
+            <h2>
+              OVERVIEW
+            </h2>
+            <p class="subtitle">
+              <div v-text="location"></div>
+            </p>
+            <p class="subtitle">
+              <div v-text="MSC"></div>
+            </p>
+            <p class="subtitle">
+              <div v-text="educationPCC"></div>
+            </p>
+            <p class="subtitle">
+              <div v-text="workChippy"></div>
+            </p>
+            <p class="subtitle">
+              <div v-text="additionalInformation"></div>
+            </p>
+            <p class="subtitle">
+              <div v-text="available"></div>
+            </p>        
+          </div>
+          <vue-particles
+            color="#b76e79"
+            :particleOpacity="0.7"
+            :particlesNumber="80"
+            shapeType="star"
+            :particleSize="4"
+            linesColor="#b76e79"
+            :linesWidth="1"
+            :lineLinked="true"
+            :lineOpacity="0.4"
+            :linesDistance="150"
+            :moveSpeed="3"
+            :hoverEffect="true"
+            hoverMode="grab"
+            :clickEffect="true"
+            clickMode="push"
+          >
+          </vue-particles> 
+        </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'About',
-    data: () => ({
-      location: `Based in Moutain Home AFB, ID`,
-      MSC: `Chapter leader with MilSpouseCoders`,
-      education: ``,
-      work: ``,        
-      educationPCC: `I hold a Computer Information Systems degree from Portland Community College.`,
-      educationCW: `I'm a graduate of Boise CodeWorks Immersive Full-Stack. `,
-      workChippy: `Owner and founder of the consulting and custom software development company Chippy Solutions. Call or Email to make an appointment for your first consultation`,
-      additionalInformation: `I am a military spouse. Coffee fiend. Dog mom. STOTT certified Pilates instructor. And long time violinist.`,
-      available: `I'm currently available to take new clients`
-      }),
-    }
-    
+  name: "About",
+  data: () => ({
+    location: `Based in Moutain Home AFB, ID`,
+    MSC: `Chapter leader with MilSpouseCoders`,
+    education: ``,
+    work: ``,
+    educationPCC: `I hold a Computer Information Systems degree from Portland Community College.`,
+    educationCW: `I'm a graduate of Boise CodeWorks Immersive Full-Stack. `,
+    workChippy: `Owner and founder of the consulting and custom software development company Chippy Solutions. Call or Email to make an appointment for your first consultation`,
+    additionalInformation: `I am a military spouse. Coffee fiend. Dog mom. STOTT certified Pilates instructor. And long time violinist.`,
+    available: `I'm currently available to take new clients`
+  })
+};
 </script>
 
 
 <style scoped>
-#inspire{
-  padding-top: 25px;
-  padding-bottom: 0px;
-  margin-bottom: 0px;
-}
-
-.bg-img {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  background: url('/src/img/location.jpg') center center no-repeat;
-  background-size: cover;
-}
-
-.border-card{
-  border: solid black thin;
-  padding: 15px;
-}
-
-.change{
-  background: white;
-}
-
-.change:hover{
-  background-color:#607d8b;
+.about {
+  background-color: black;
   color: white;
-  cursor: pointer;
-  -webkit-transition: background-color 2s ease-out;
-  -moz-transition: background-color 2s ease-out;
-  -o-transition: background-color 2s ease-out;
-  transition: background-color 2s ease-out;
+  padding-bottom: 100px;
+  padding-top: 20px;
 }
 
+.subtitle{
+  border-top: solid #b76e79 thin;
+  padding: 20px;
+}
+  .wrap-banner {
+    position: relative;
+    -webkit-align-items: center;
+    -ms-flex-align: center;
+    align-items: center;
+    display: -webkit-flex;
+    display: inline-flex;
+    /* height: 100%; */
+    justify-content: center;
+  }
+
+.main-section {
+  max-width: 650px;
+  margin: auto;
+  padding: 0 1rem;
+}
 
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -22,28 +22,9 @@
         </span>
       </div>
     </nav>
-
-      <vue-particles
-        color="#dedede"
-        :particleOpacity="0.7"
-        :particlesNumber="80"
-        shapeType="star"
-        :particleSize="4"
-        linesColor="#dedede"
-        :linesWidth="1"
-        :lineLinked="true"
-        :lineOpacity="0.4"
-        :linesDistance="150"
-        :moveSpeed="3"
-        :hoverEffect="true"
-        hoverMode="grab"
-        :clickEffect="true"
-        clickMode="push"
-      >
     <about></about>
     <project></project>
     <contact></contact>
-      </vue-particles>
   <gmap-map
     :center="center"
     :zoom="10"
